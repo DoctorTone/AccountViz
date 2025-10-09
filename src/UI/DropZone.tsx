8f7c191e7a49e10cbb361302ef92b8d4fe79adc5
DoctorTone <tony@drt-software.com>
2025-10-09 17:08:50 +0100

    Implemented file drag and drop

--- src/UI/DropZone.tsx
+++ src/UI/DropZone.tsx
@@ -1,9 +1,25 @@
+import { useEffect } from "react";
 import Typography from "@mui/material/Typography";
 
 const DropZone = () => {
+  const handleDrop = (event) => {
+    event.preventDefault();
+    const droppedFiles = event.dataTransfer.files;
+    if (droppedFiles.length > 0) {
+      const newFiles = Array.from(droppedFiles);
+      console.log("new files = ", newFiles[0]);
+    }
+  };
+
   return (
-    <div id="dropZone" className="panel">
+    <div
+      id="dropZone"
+      className="panel"
+      onDrop={handleDrop}
+      onDragOver={(event) => event.preventDefault()}
+    >
       <Typography variant="h5">Drag your file here.</Typography>
+      <input type="file" id="fileInput" accept=".csv" />
     </div>
   );
 };
