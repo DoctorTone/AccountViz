227cf8d41c95459d706718051dc4415c35a0cb51
DoctorTone <tony@drt-software.com>
2025-10-10 16:38:33 +0100

    Populate table with real data

--- src/dialogs/DataDialog.tsx
+++ src/dialogs/DataDialog.tsx
@@ -10,6 +10,7 @@ import useStore from "../state/store";
 const DataDialog = () => {
   const dataLoaded = useStore((state) => state.dataLoaded);
   const setDataLoaded = useStore((state) => state.setDataLoaded);
+  const rows = useStore((state) => state.rows);
 
   const handleClose = () => {
     setDataLoaded(false);
@@ -17,35 +18,19 @@ const DataDialog = () => {
 
   const columns: GridColDef[] = [
     { field: "id", headerName: "ID", width: 70 },
-    { field: "firstName", headerName: "First name", width: 130 },
-    { field: "lastName", headerName: "Last name", width: 130 },
+    { field: "date", headerName: "Date", width: 130 },
+    { field: "type", headerName: "Type", width: 130 },
     {
-      field: "age",
-      headerName: "Age",
+      field: "amount",
+      headerName: "Amount",
       type: "number",
       width: 90,
     },
     {
-      field: "fullName",
-      headerName: "Full name",
-      description: "This column has a value getter and is not sortable.",
-      sortable: false,
+      field: "description",
+      headerName: "Description",
       width: 160,
-      valueGetter: (value, row) =>
-        `${row.firstName || ""} ${row.lastName || ""}`,
     },
-  ];
-
-  const rows = [
-    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
   ];
 
   const paginationModel = { page: 0, pageSize: 5 };
