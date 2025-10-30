5382ea703c6988638e60faee18bb2fe2f612c690
DoctorTone <tony@drt-software.com>
2025-10-30 14:24:15 +0000

    Scaled to max value

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -9,17 +9,25 @@ interface BarChartProps {
 }
 
 export const BarChart = ({ data }: BarChartProps) => {
-  let totalsOut = Object.fromEntries(
+  const totalsOut = Object.fromEntries(
     Object.entries(data).filter(([_, value]) => value < -0.1)
   );
 
+  // Get max (of negative values)
+  const values = Object.values(totalsOut);
+  const max = Math.min(...values) * -1;
+  const scale = max / 10;
   // DEBUG
   console.log("Totals = ", totalsOut);
+  console.log("Max = ", max);
   return (
     <>
       {Object.entries(totalsOut).map(([_, value], index) => {
         return (
-          <Cylinder position={[index * 4, -value / 2, 0]} args={[1, 1, -value]}>
+          <Cylinder
+            position={[index * 4, -value / scale / 2, 0]}
+            args={[0.5, 0.5, -value / scale]}
+          >
             <meshStandardMaterial color={"red"} />
           </Cylinder>
         );
