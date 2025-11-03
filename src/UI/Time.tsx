044d84d9ee05bbfaf21a18d5375bc9c002683698
DoctorTone <tony@drt-software.com>
2025-11-03 17:28:37 +0000

    Formatted time UI

--- src/UI/Time.tsx
+++ src/UI/Time.tsx
@@ -2,6 +2,7 @@ import Typography from "@mui/material/Typography";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import IconButton from "@mui/material/IconButton";
+import Box from "@mui/material/Box";
 import useStore from "../state/store";
 
 const Time = () => {
@@ -12,12 +13,14 @@ const Time = () => {
   return (
     <div id="period" className="panel">
       <Typography variant="h6">Period: {currentPeriod}</Typography>
-      <IconButton onClick={() => selectPrevMonth()}>
-        <ArrowBackIosIcon />
-      </IconButton>
-      <IconButton onClick={() => selectNextMonth()}>
-        <ArrowForwardIosIcon />
-      </IconButton>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <IconButton onClick={() => selectPrevMonth()}>
+          <ArrowBackIosIcon />
+        </IconButton>
+        <IconButton onClick={() => selectNextMonth()}>
+          <ArrowForwardIosIcon />
+        </IconButton>
+      </Box>
     </div>
   );
 };
